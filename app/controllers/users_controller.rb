c18354216c5e2de1df804f6c8503eaf027bc048a
EC2 Default User <ec2-user@ip-172-31-16-44.ap-northeast-1.compute.internal>
2023-09-10 14:38:52 +0000

    [Add]プロフィール画像保存機能実装中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :image)
   end
 
 end
