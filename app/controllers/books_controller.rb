40e96dd48bb2e074b40a167208ff052a584f474d
EC2 Default User <ec2-user@ip-172-31-16-44.ap-northeast-1.compute.internal>
2023-09-14 12:42:01 +0000

    [Add]バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -34,12 +35,12 @@ class BooksController < ApplicationController
     unless @book.user.id == current_user.id
       redirect_to user_path
     end
-    book = Book.find(params[:id])
-    if book.update(book_params)
+
+    if @book.update(book_params)
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(book.id)
     else
-
+      render :edit
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,11 @@ class UsersController < ApplicationController
       redirect_to user_path
     end
 
-    user = User.find(params[:id])
-    if user.update(user_params)
+    if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(user.id)
     else
-
+      render :edit
     end
 
   end
